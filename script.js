1a5dcda80bd5593f0d8e884e279e10e97afab9fd
Cadence Higgins <cadencehiggins@gmail.com>
2024-05-01 10:52:58 -0500

    completed api calls, appending all info to screen

--- script.js
+++ script.js
@@ -7,24 +7,19 @@ function typeChoice(el) {
     return chosenType;
 }
 
-function participantsChoice(el) {
-    const chosenParticipants = el
-    const displayBox = document.getElementById('participants-choice')
-    displayBox.textContent = chosenParticipants
-    return chosenParticipants;
-}
+
+let moneyNum
 
 function moneyChoice(el) {
     const chosenMoney = el
     const displayBox = document.getElementById('money-choice')
     displayBox.textContent = chosenMoney
 
-    if (chosenMoney === no) {
-        const noMoney = 0
-        return noMoney;
-    } else {
-        const yesMoney = 1
-        return yesMoney;
+    if (chosenMoney === 'no') {
+        moneyNum = 'price=0'
+    }
+    if(chosenMoney === 'yes') {
+        moneyNum = 'minprice=0&maxprice=0.1'
     }
 
 }
@@ -32,13 +27,12 @@ function moneyChoice(el) {
 async function response() {
     try {
         const apiType = document.getElementById('type-choice').textContent
-        const apiParticipants = document.getElementById('participants-choice').textContent
-        const apiCall = await fetch(`https://www.boredapi.com/api/activity?type=${apiType}&participants=${apiParticipants}`)
+        const apiCall = await fetch(`https://www.boredapi.com/api/activity?type=${apiType}&${moneyNum}`)
 
         const responseJson = await apiCall.json()
-        console.log(apiType)
         console.log(responseJson)
-
+        const activityDisplay = document.getElementById('info-box')
+        activityDisplay.textContent = responseJson.activity
     }
     catch (error) {
         console.error(error)
